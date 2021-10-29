3b2f4bd250021beb60a1fb66decf4fbb4017837f
ii887522 <ii887522@gmail.com>
2021-10-29 15:49:36 +0800

    Fix lint error

--- index.js
+++ index.js
@@ -7,12 +7,11 @@ const hostnameIndex = 3;
 async function update() {
     const ipv6AddressPromise = axios.get('https://ipv6bot.whatismyipaddress.com');
     const dynv6ApiEndpoint = 'https://dynv6.com/api/v2';
-    const zone = await axios.get(`${dynv6ApiEndpoint}/zones/by-name/${process.argv[hostnameIndex]}`, { headers: { 'Authorization': `Bearer ${process.argv[httpTokenIndex]}` } });
+    const zone = await axios.get(`${dynv6ApiEndpoint}/zones/by-name/${process.argv[hostnameIndex] ?? ''}`, { headers: { Authorization: `Bearer ${process.argv[httpTokenIndex] ?? ''}` } });
     const ipv6Address = (await ipv6AddressPromise).data;
-    axios.patch(`${dynv6ApiEndpoint}/zones/${zone.data.id}`, { ipv6prefix: ipv6Address }, { headers: { 'Authorization': `Bearer ${process.argv[httpTokenIndex]}` } })
-        .then(() => console.log(`${new Date().toLocaleTimeString()}: ${process.argv[hostnameIndex]} address updated`));
-    (await axios.get(`${dynv6ApiEndpoint}/zones/${zone.data.id}/records`, { headers: { 'Authorization': `Bearer ${process.argv[httpTokenIndex]}` } })).data
-        .filter((record) => record.name !== '').forEach((record) => axios.patch(`${dynv6ApiEndpoint}/zones/${zone.data.id}/records/${record.id}`, { data: ipv6Address }, { headers: { 'Authorization': `Bearer ${process.argv[httpTokenIndex]}` } }).then(response => console.log(`${new Date().toLocaleTimeString()}: ${response.data.name}.${process.argv[hostnameIndex]} address updated`)));
+    consume(axios.patch(`${dynv6ApiEndpoint}/zones/${zone.data.id}`, { ipv6prefix: ipv6Address }, { headers: { Authorization: `Bearer ${process.argv[httpTokenIndex] ?? ''}` } }).then(() => console.log(`${new Date().toLocaleTimeString()}: ${process.argv[hostnameIndex] ?? ''} address updated`)));
+    (await axios.get(`${dynv6ApiEndpoint}/zones/${zone.data.id}/records`, { headers: { Authorization: `Bearer ${process.argv[httpTokenIndex] ?? ''}` } })).data
+        .filter((record) => record.name !== '').forEach(async (record) => await axios.patch(`${dynv6ApiEndpoint}/zones/${zone.data.id}/records/${record.id}`, { data: ipv6Address }, { headers: { Authorization: `Bearer ${process.argv[httpTokenIndex] ?? ''}` } }).then(response => console.log(`${new Date().toLocaleTimeString()}: ${response.data.name}.${process.argv[hostnameIndex] ?? ''} address updated`)));
 }
 consume(update());
-setInterval(update, 300000);
+setInterval(() => consume(update()), 300000);
--- index.ts
+++ index.ts
@@ -8,21 +8,27 @@ import { consume } from '@ii887522/hydro'
 const httpTokenIndex = 2
 const hostnameIndex = 3
 
-async function update () {
+async function update (): Promise<void> {
   const ipv6AddressPromise = axios.get('https://ipv6bot.whatismyipaddress.com')
   const dynv6ApiEndpoint = 'https://dynv6.com/api/v2'
   const zone = await axios.get(
-    `${dynv6ApiEndpoint}/zones/by-name/${process.argv[hostnameIndex]}`, { headers: { 'Authorization': `Bearer ${process.argv[httpTokenIndex]}` } }
-  );
+    `${dynv6ApiEndpoint}/zones/by-name/${process.argv[hostnameIndex] ?? ''}`, { headers: { Authorization: `Bearer ${process.argv[httpTokenIndex] ?? ''}` } }
+  )
   const ipv6Address = (await ipv6AddressPromise).data
-  axios.patch(`${dynv6ApiEndpoint}/zones/${zone.data.id}`, { ipv6prefix: ipv6Address }, { headers: { 'Authorization': `Bearer ${process.argv[httpTokenIndex]}` } })
-    .then(() => console.log(`${new Date().toLocaleTimeString()}: ${process.argv[hostnameIndex]} address updated`));
-  (await axios.get(`${dynv6ApiEndpoint}/zones/${zone.data.id}/records`, { headers: { 'Authorization': `Bearer ${process.argv[httpTokenIndex]}` } })).data
-    .filter((record: { name: string }) => record.name !== '').forEach((record: { id: any }) => axios.patch(
-      `${dynv6ApiEndpoint}/zones/${zone.data.id}/records/${record.id}`, { data: ipv6Address }, { headers: { 'Authorization': `Bearer ${process.argv[httpTokenIndex]}` } }
-    ).then(response => console.log(`${new Date().toLocaleTimeString()}: ${response.data.name}.${process.argv[hostnameIndex]} address updated`)))
+  consume(
+    axios.patch(
+      `${dynv6ApiEndpoint}/zones/${zone.data.id as number}`,
+      { ipv6prefix: ipv6Address },
+      { headers: { Authorization: `Bearer ${process.argv[httpTokenIndex] ?? ''}` } }
+    ).then(() => console.log(`${new Date().toLocaleTimeString()}: ${process.argv[hostnameIndex] ?? ''} address updated`))
+  );
+  (await axios.get(`${dynv6ApiEndpoint}/zones/${zone.data.id as number}/records`, { headers: { Authorization: `Bearer ${process.argv[httpTokenIndex] ?? ''}` } })).data
+    .filter((record: { name: string }) => record.name !== '').forEach(async (record: { id: any }) => await axios.patch(
+      `${dynv6ApiEndpoint}/zones/${zone.data.id as number}/records/${record.id as number}`,
+      { data: ipv6Address },
+      { headers: { Authorization: `Bearer ${process.argv[httpTokenIndex] ?? ''}` } }
+    ).then(response => console.log(`${new Date().toLocaleTimeString()}: ${response.data.name as string}.${process.argv[hostnameIndex] ?? ''} address updated`)))
 }
 
-
 consume(update())
-setInterval(update, 300000)
+setInterval(() => consume(update()), 300000)
