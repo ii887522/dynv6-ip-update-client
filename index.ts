17453d030b5d11939c6773df05b95b300cff2f09
ii887522 <ii887522@gmail.com>
2022-10-02 17:03:37 +0800

    Configure project to use ESLint and Prettier

--- index.ts
+++ index.ts
@@ -9,45 +9,73 @@ import publicIp from 'public-ip'
 import constants from './src/constants.js'
 import Zone from './src/Zone'
 
-axiosRetry(
-  axios, { retries: 16, retryDelay: exponentialDelay, retryCondition: error => error.response === undefined || (error.response.status >= 400 && error.response.status < 600) }
-)
+axiosRetry(axios, {
+  retries: 16,
+  retryDelay: exponentialDelay,
+  retryCondition: error =>
+    error.response === undefined || (error.response.status >= 400 && error.response.status < 600),
+})
 
-async function updateRecords (zone: Zone, ipv6Address: string): Promise<void> {
-  for (
-    const record of (await axios.get(
-      `${constants.dynv6ApiEndpoint}/zones/${zone.id}/records`,
-      { headers: { Authorization: `Bearer ${process.argv[constants.httpTokenIndex] ?? ''}` } }
-    )).data
-  ) {
+async function updateRecords(zone: Zone, ipv6Address: string): Promise<void> {
+  for (const record of (
+    await axios.get(`${constants.dynv6ApiEndpoint}/zones/${zone.id}/records`, {
+      headers: {
+        Authorization: `Bearer ${process.argv[constants.httpTokenIndex] ?? ''}`,
+      },
+    })
+  ).data) {
     consume(
-      axios.patch(
-        `${constants.dynv6ApiEndpoint}/zones/${zone.id}/records/${record.id as number}`,
-        { data: ipv6Address },
-        { headers: { Authorization: `Bearer ${process.argv[constants.httpTokenIndex] ?? ''}` } }
-      ).then(response => console.log(`${new Date().toLocaleTimeString()}: ${response.data.name as string}${response.data.name === '' ? '' : '.'}${zone.name} address updated`))
+      axios
+        .patch(
+          `${constants.dynv6ApiEndpoint}/zones/${zone.id}/records/${record.id as number}`,
+          { data: ipv6Address },
+          {
+            headers: {
+              Authorization: `Bearer ${process.argv[constants.httpTokenIndex] ?? ''}`,
+            },
+          }
+        )
+        .then(response =>
+          console.log(
+            `${new Date().toLocaleTimeString()}: ${response.data.name as string}${
+              response.data.name === '' ? '' : '.'
+            }` + `${zone.name} address updated`
+          )
+        )
     )
   }
 }
 
-function updateZone (zone: Zone, ipv6Address: string): void {
+function updateZone(zone: Zone, ipv6Address: string): void {
   consume(
     axios.patch(
       `${constants.dynv6ApiEndpoint}/zones/${zone.id}`,
       { ipv6prefix: ipv6Address },
-      { headers: { Authorization: `Bearer ${process.argv[constants.httpTokenIndex] ?? ''}` } }
+      {
+        headers: {
+          Authorization: `Bearer ${process.argv[constants.httpTokenIndex] ?? ''}`,
+        },
+      }
     )
   )
   consume(updateRecords(zone, ipv6Address))
 }
 
-async function update (): Promise<void> {
-  const ipv6AddressPromise = publicIp.v6()
-  for (const zone of (await axios.get(`${constants.dynv6ApiEndpoint}/zones`, { headers: { Authorization: `Bearer ${process.argv[constants.httpTokenIndex] ?? ''}` } })).data) {
+async function update(): Promise<void> {
+  const ipv6AddressPromise = publicIp.publicIpv6()
+  for (const zone of (
+    await axios.get(`${constants.dynv6ApiEndpoint}/zones`, {
+      headers: {
+        Authorization: `Bearer ${process.argv[constants.httpTokenIndex] ?? ''}`,
+      },
+    })
+  ).data) {
     updateZone(zone, await ipv6AddressPromise)
   }
 }
 
-if (process.argv.length !== constants.requiredCommandLineArgCount) throw new Error('There must be exactly 1 command line argument passed in! Please try again.')
+if (process.argv.length !== constants.requiredCommandLineArgCount) {
+  throw new Error('There must be exactly 1 command line argument passed in! Please try again.')
+}
 consume(update())
 setInterval(() => consume(update()), 300000)
